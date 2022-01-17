372e4659c64a7bdda85ab2314cf07f4efd35cb53
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-01-17 19:50:20 +0100

    [add component and set routing]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LawsuitsComponent } from './lawsuits/lawsuits.component';
+
+const appRoutes: Routes = [
+  { path: 'lawsuits', component: LawsuitsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LawsuitsComponent
   ],
   imports: [
     BrowserModule
