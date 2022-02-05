6522554d7df09cd9ab5b751a721b2e2d7bee455f
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-02-05 19:26:52 +0100

    feature(services): add task service

---
+++ src/app/tasks/task.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import {Contact} from "../shared/contact.model";
+import {Address} from "../shared/address.model";
+import {Task} from "../shared/task.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private contacts: Contact[] = [
+    new Contact(
+      '1',
+      'NAME',
+      'FirstName',
+      'LastName',
+      'email@email.com',
+      'altEmail@email.com',
+      '555-555-555',
+      '555-555-556',
+      'company INC',
+      '77111111111',
+      '123456',
+      '321654',
+      '225588',
+      '12-11-2012',
+      '12-12-2012',
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
+    ),
+    new Contact(
+      '2',
+      'NAME',
+      'FirstName',
+      'LastName',
+      'email@email.com',
+      'altEmail@email.com',
+      '555-555-555',
+      '555-555-556',
+      'company INC',
+      '77111111111',
+      '123456',
+      '321654',
+      '225588',
+      '12-11-2012',
+      '12-12-2012',
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
+    ),
+    new Contact(
+      '3',
+      'NAME',
+      'FirstName',
+      'LastName',
+      'email@email.com',
+      'altEmail@email.com',
+      '555-555-555',
+      '555-555-556',
+      'company INC',
+      '77111111111',
+      '123456',
+      '321654',
+      '225588',
+      '12-11-2012',
+      '12-12-2012',
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
+      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
+    )
+  ];
+  private tasks: Task[] = [
+    new Task('1', true, '14-11-2022', 'some task',this.contacts.slice()),
+    new Task('2', true, '15-11-2022', 'some task',this.contacts.slice()),
+    new Task('3', true, '16-11-2022', 'some task',this.contacts.slice())
+  ];
+
+  constructor() { }
+
+  getTasks() {
+    return this.tasks.slice();
+  }
+}
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from "../../shared/task.model";
-import {Contact} from "../../shared/contact.model";
-import {Address} from "../../shared/address.model";
+import {TaskService} from "../task.service";
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,74 +8,12 @@ import {Address} from "../../shared/address.model";
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
-  contacts: Contact[] = [
-    new Contact(
-      '1',
-      'NAME',
-      'FirstName',
-      'LastName',
-      'email@email.com',
-      'altEmail@email.com',
-      '555-555-555',
-      '555-555-556',
-      'company INC',
-      '77111111111',
-      '123456',
-      '321654',
-      '225588',
-      '12-11-2012',
-      '12-12-2012',
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
-    ),
-    new Contact(
-      '2',
-      'NAME',
-      'FirstName',
-      'LastName',
-      'email@email.com',
-      'altEmail@email.com',
-      '555-555-555',
-      '555-555-556',
-      'company INC',
-      '77111111111',
-      '123456',
-      '321654',
-      '225588',
-      '12-11-2012',
-      '12-12-2012',
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
-    ),
-    new Contact(
-      '3',
-      'NAME',
-      'FirstName',
-      'LastName',
-      'email@email.com',
-      'altEmail@email.com',
-      '555-555-555',
-      '555-555-556',
-      'company INC',
-      '77111111111',
-      '123456',
-      '321654',
-      '225588',
-      '12-11-2012',
-      '12-12-2012',
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska'),
-      new Address('id','Street 122', 'Warszawa', '00-000', 'Polska')
-    )
-  ];
-  tasks: Task[] = [
-    new Task('1', true, '14-11-2022', 'some task',this.contacts.slice()),
-    new Task('2', true, '15-11-2022', 'some task',this.contacts.slice()),
-    new Task('3', true, '16-11-2022', 'some task',this.contacts.slice())
-  ];
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
   }
 
 }
