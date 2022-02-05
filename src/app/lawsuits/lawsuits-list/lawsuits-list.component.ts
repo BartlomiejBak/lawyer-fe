162a29829c293c1af63433c08c3a5c68c1e7cd0e
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-02-05 18:13:22 +0100

    feature(services): add lawsuit service

--- src/app/lawsuits/lawsuits-list/lawsuits-list.component.ts
+++ src/app/lawsuits/lawsuits-list/lawsuits-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ContactRole } from 'src/app/shared/contact-role.model';
-import { Lawsuit } from 'src/app/shared/lawsuit.model';
+import {Component, OnInit} from '@angular/core';
+import {Lawsuit} from 'src/app/shared/lawsuit.model';
+import {LawsuitService} from "../lawsuit.service";
 
 @Component({
   selector: 'app-lawsuits-list',
@@ -8,16 +8,12 @@ import { Lawsuit } from 'src/app/shared/lawsuit.model';
   styleUrls: ['./lawsuits-list.component.css']
 })
 export class LawsuitsListComponent implements OnInit {
-  lawsuits: Lawsuit[] = [
-    new Lawsuit('1', 'first lawsuit', 'defender', '11-06-2021', '11-06-2021', 'aa01', 500000, 'test lawsuit', [], [], []),
-    new Lawsuit('2', 'second lawsuit', 'defender', '11-06-2021', '11-06-2021', 'aa01', 500000, 'test lawsuit', [], [], []),
-    new Lawsuit('3', 'third lawsuit', 'defender', '11-06-2021', '11-06-2021', 'aa01', 500000, 'test lawsuit', [], [], []),
-    new Lawsuit('4', 'fourth lawsuit', 'defender', '11-06-2021', '11-06-2021', 'aa01', 500000, 'test lawsuit', [], [], [])
-  ];
+  lawsuits: Lawsuit[] = [];
 
-  constructor() {}
+  constructor(private lawsuitService: LawsuitService) {}
 
   ngOnInit(): void {
+    this.lawsuits = this.lawsuitService.getLawsuits();
   }
 
 }
