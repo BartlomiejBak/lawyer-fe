0e9af9f8c12063cebddc428a1d44ecc48e7664c7
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-02-05 18:31:20 +0100

    feature(services): add note service

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Note} from "../../shared/note.model";
+import {NoteService} from "../note.service";
 
 @Component({
   selector: 'app-notes-list',
@@ -7,15 +8,12 @@ import {Note} from "../../shared/note.model";
   styleUrls: ['./notes-list.component.css']
 })
 export class NotesListComponent implements OnInit {
-  notes: Note[] = [
-    new Note('1', 'title 1', 'text 1'),
-    new Note('2', 'title 2', 'text 2'),
-    new Note('3', 'title 3', 'text 3'),
-  ];
+  notes: Note[] = [];
 
-  constructor() { }
+  constructor(private noteService: NoteService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.notes = this.noteService.getNotes();
   }
 
 }
