959f0af539573e6a349839fb4f625361dafbbaa6
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-03-05 15:00:17 +0100

    feature(edit): add mock edit contact component

--- src/app/contacts/contacts-list/contacts-list.component.ts
+++ src/app/contacts/contacts-list/contacts-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Contact} from "../../shared/contact.model";
 import {Address} from "../../shared/address.model";
 import {ContactService} from "../contact.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-contacts-list',
@@ -11,10 +12,17 @@ import {ContactService} from "../contact.service";
 export class ContactsListComponent implements OnInit {
   contacts: Contact[] = [];
 
-  constructor(private contactService: ContactService) { }
+  constructor(private contactService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
   }
+
+  onNewContact() {
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
+
 
 }
