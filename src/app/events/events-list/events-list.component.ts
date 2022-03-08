4a0d89082ee92b036c82b8bb8c51c8d72fcc611e
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-03-08 16:54:00 +0100

    feature(edit): add mock edit event component

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Event} from "../../shared/event.model";
 import {EventService} from "../event.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-events-list',
@@ -10,10 +11,17 @@ import {EventService} from "../event.service";
 export class EventsListComponent implements OnInit {
   events: Event[] = [];
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
   }
+
+  onNewEvent() {
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
+
 
 }
