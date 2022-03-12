5a029c00cd1d91284c1456046c27d042ffc3f526
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-03-12 10:58:47 +0100

    feature(edit): add mock edit poa component

--- src/app/poas/poas-list/poas-list.component.ts
+++ src/app/poas/poas-list/poas-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Poa} from "../../shared/poa.model";
 import {PoaService} from "../poa.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-poas-list',
@@ -10,10 +11,16 @@ import {PoaService} from "../poa.service";
 export class PoasListComponent implements OnInit {
   poas: Poa[] = [];
 
-  constructor(private poaService: PoaService) { }
+  constructor(private poaService: PoaService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.poas = this.poaService.getPoas();
+  }
+
+  onNewPoa() {
+    this.router.navigate(['new'], {relativeTo: this.route})
   }
 
 }
